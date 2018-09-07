5663d7a4ba30d636167f1085500b4e81b2a1c0c7
angelolvera00 <A01411230@itesm.mx>
2018-09-06 20:41:35 -0500

    commit

---
+++ cmake-build-debug/CMakeFiles/Years.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Years.dir/main.c.obj"
+  "Years.pdb"
+  "Years.exe"
+  "Years.exe.manifest"
+  "libYears.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Years.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
